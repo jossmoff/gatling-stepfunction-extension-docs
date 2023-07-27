74c9614ffa4a109a7c86a4ccf03acf5bf326c5a1
jossmoff <josshmoffatt@gmail.com>
2023-07-28 00:04:13 +0100

    :tada: Starting documentation project

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,30 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: '',
+			logo: {
+				src: './src/assets/gatling-sfn-ext.png',
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/jossmoff/gatling-stepfunction-extension',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Getting Started',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ label: 'Installation', link: '/getting-started/installation/' },
+						{ label: 'Quickstart', link: '/getting-started/quickstart/' },
 					],
 				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Standard vs. Express Workflows', link: '/reference/standard-vs-express/' },
+						{ label: 'Start Executions', link: '/reference/start-executions/' },
+						{ label: 'Check Execution Success', link: '/reference/check-execution-success/' },
+					]
 				},
 			],
 		}),
