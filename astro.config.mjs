2c0630e304f9da15bb53ad3a37f3e1af7714d7d3
jossmoff <josshmoffatt@gmail.com>
2023-07-31 21:46:08 +0100

    Remove redundant info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
